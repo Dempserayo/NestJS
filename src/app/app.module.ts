877727fd1a97b360e415c26c6bc50f3257204130
Raye <160432415+Dempserayo@users.noreply.github.com>
2026-02-24 17:21:52 -0500

    Refactor project structure by moving Users and Product controllers and services to a new modules directory; remove unused controller and service files for improved organization and clarity.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { ProductController } from './product/product.controller';
-import { UsersService } from './users/users.service';
+import { UsersController } from './modules/users/users.controller';
+import { ProductController } from './modules/product/product.controller';
+import { UsersService } from './modules/users/users.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
 
